4ba3675c1f3ef4dd94e39ccd7e180b0a2e917fb2
Vanessa <vanessa.suazo@cl.ey.com>
2025-02-27 15:56:05 -0300

    Updates

--- assets/md.transition-legacy-qpJ2_gML.js
+++ assets/md.transition-legacy-qpJ2_gML.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-O0k8bXSh.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-XsSO8Vb0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
