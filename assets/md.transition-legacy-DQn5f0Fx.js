a136243e09b4e5201b7a43a68880b52df3adfd30
Vanessa <vanessa.suazo@cl.ey.com>
2025-02-27 15:34:37 -0300

    Updates

--- assets/md.transition-legacy-DQn5f0Fx.js
+++ assets/md.transition-legacy-DQn5f0Fx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-mvWNDDAL.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BC6TV3k-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BoJx0PRm.js
+++ assets/swipe-back-legacy-BoJx0PRm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-mvWNDDAL.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BC6TV3k-.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
